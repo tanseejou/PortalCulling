491b7412879cc7e0e1bac3f1a6c17181041b3af8
tanseejou <tanseejou97@gmail.com>
2020-05-08 04:41:36 +0800

    Project Upload

---
+++ PortalCulling_02/Assets/Scripts/FramesPerSecond.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FramesPerSecond : MonoBehaviour
+{
+    Rect fpsRect;
+    Rect commandRect;
+    float fps;
+    GUIStyle style;
+    GUIStyle commandStyle;
+
+    void Start()
+    {
+        fpsRect = new Rect(20, 20, 400, 200);
+        commandRect = new Rect(970, 270, 400, 200);
+        style = new GUIStyle();
+        style.fontSize = 18;
+        style.normal.textColor = Color.white;
+
+        //commandStyle = new GUIStyle();
+        //commandStyle.fontSize = 18;
+        //commandStyle.normal.textColor = Color.white;
+
+        StartCoroutine(RecalculatorFPS());
+    }
+    
+    private IEnumerator RecalculatorFPS()
+    {
+        while(true)
+        {
+            fps = 1 / Time.deltaTime;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void OnGUI()
+    {
+        
+    //    GUI.Label(fpsRect, "FPS: " + string.Format("{0:0.0}", fps), style);
+       
+        GUI.Label(commandRect, "WSAD - movement", style);
+        GUI.Label(commandRect, "\nMouse - view rotation", style);
+        
+    }
+
+
+}
---
+++ PortalCulling_02/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    public Camera eyes;
+    public float speed;
+    public float sensitivity;
+    float moveFB;
+    float moveLR;
+    float mouseX;
+    float mouseY; 
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveFB = Input.GetAxis("Vertical");
+        moveLR = Input.GetAxis("Horizontal");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        transform.Rotate(0, mouseX*sensitivity, 0);
+        Vector3 movement = new Vector3(moveFB * speed * Time.deltaTime, 0, moveLR * speed * Time.deltaTime);
+        controller.Move(transform.rotation*movement);
+        eyes.transform.Rotate(-mouseY*sensitivity, 0, 0);
+  
+    }
+}
